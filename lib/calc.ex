d0311945a78554d2ad017c0d506367ea23653273
Michael Ries <michael@riesd.com>
2018-03-15 12:31:34 -0600

    initial

---
+++ lib/calc.ex
@@ -0,0 +1,18 @@
+defmodule Calc do
+  @moduledoc """
+  Documentation for Calc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Calc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
